d1642b76ed45932ebb4a6dd25c55a54f2675f15f
Jean-Charles Sisk <jasisk@gmail.com>
2018-02-07 08:22:01 -0500

    operate in memory

--- precolor.c
+++ precolor.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#include <stddef.h>
+// #include <stddef.h>
 #include <stdlib.h>
 
 #define BUFFERSIZE 2048
-#define DELIMITER "\t"
+#define DELIMITER '\t'
 
 #define RESET   "\x1B[0m"
 #define RED     "\x1B[31m"
@@ -15,21 +15,27 @@
 #define CYAN    "\x1B[36m"
 #define WHITE   "\x1B[37m"
 
-const char *a[]={RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
-const unsigned long len=sizeof(a)/sizeof(*a);
+const char *colors[]={RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
+const unsigned long colors_length=sizeof(colors)/sizeof(*colors);
 
 int main(int argc, char* argv[]) {
   char buffer[BUFFERSIZE];
-  int num_of_entries=0;
+  size_t num_of_entries=0;
 
   char **prefixes = malloc(num_of_entries * sizeof(char*));
 
   while (fgets(buffer, BUFFERSIZE, stdin) != NULL) {
+    puts("ugh");
     char *cp = &buffer[0];
-    char *token = strsep(&cp, DELIMITER);
-    int i;
+    char *token = strchr(buffer, DELIMITER);
+    if (token == NULL) {
+      fputs(buffer, stdout);
+      continue;
+    }
+    size_t len = token-cp;
+    size_t i;
     for (i = 0; i < num_of_entries;) {
-      if (!strcmp(prefixes[i], token)) {
+      if (!memcmp(prefixes[i], cp, len)) {
         token = prefixes[i];
         break;
       }
@@ -37,11 +43,14 @@ int main(int argc, char* argv[]) {
     }
     if (i == num_of_entries) {
       prefixes = realloc(prefixes, (num_of_entries + 1) * sizeof(char*));
-      prefixes[num_of_entries++] = strdup(token);
+      token = malloc(len+1);
+      memcpy(token, cp, len);
+      token[len+1] = '\0';
+      prefixes[num_of_entries++] = token;
     }
-    printf("%s%s" RESET DELIMITER "%s", a[i % len], token, cp);
+    printf("%s%s" RESET "%s", colors[i % colors_length], token, cp+len);
   }
 
-  free(prefixes);
+  // free(prefixes);
   return 0;
 }
